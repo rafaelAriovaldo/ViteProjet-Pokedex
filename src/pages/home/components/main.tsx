b97e4ca4e19be05029317c75603bda12267d34a2
RafaelAriovaldo <rafaelariovaldo02@gmail.com>
2022-12-08 21:44:57 -0300

    update code houver.

--- src/pages/home/components/main.tsx
+++ src/pages/home/components/main.tsx
@@ -7,9 +7,9 @@ export function Main() {
             <div className=' bg-white w-[897px] h-[522px] border rounded-2xl justify-center ml-[1rem] mt-[2rem]  flex'>
                 <nav className=" mr-[0rem] p-14 font-Im-feel text-2xl">
                     <div className="flex gap-40">
-                        <h1 className=""><Link to={'/pokemom/register'} className="font-Im fell Englesh">Cadastre</Link></h1>
-                        <h1 className=""><a href="">Crie seu time</a></h1>
-                        <h1 className=""><a href="/abc">Pesquise</a></h1>
+                        <h1 className=""><Link to={'/pokemom/register'} className="font-Im fell Englesh hover:bg-slate-100 rounded-lg">Cadastre</Link></h1>
+                        <h1 className=""><a href="" className="hover:bg-slate-100 rounded-lg">Crie seu time</a></h1>
+                        <h1 className=""><a href="/abc" className="hover:bg-slate-100 rounded-lg">Pesquise</a></h1>
                     </div>
                     <h2 className="flex justify-center pt-[10rem] text-8xl">POKEDEX</h2>
                 </nav>
--- src/pages/registration/components/main.tsx
+++ src/pages/registration/components/main.tsx
@@ -1,34 +1,40 @@
+import { Link } from "react-router-dom";
+
 export function Main() {
     return (
         <div className=' bg-red-600 w-[1113px] h-[559px] border rounded-2xl justify-center ml-[25rem] mt-[3rem] flex '>
 
-            <form action="/cadastroDePokomon" method="post" >
+            <form action="/registerPokmeon" method="post" >
                 <fieldset className=' bg-white w-[997px] h-[522px] border rounded-2xl justify-center ml-[1rem] mt-[2rem] flex '>
 
-                    <label className=" grid m-1 items-center  w-[797px] h-[422px]  ">
+                    <label className=" grid  items-center  w-[797px] h-[422px]   ">
 
-                        <label className="w-[14rem] h-[2rem] bg-gray-300 mt-10 font-inter text-2xl ml-[21rem] pl-12">Cadastro</label>
-                        <label>
+                        <label className="w-[14rem] h-[2rem] bg-gray-300 mt-10 font-inter text-2xl ml-[21rem] flex justify-center rounded-sm ">Cadastro</label>
+                        <label className="font-inter text-xl">
                             Tipo:
-                            <input type="text" name="name" />
+                            <input className="bg-zinc-300 w-[35rem] ml-[10rem]  " type="text" name="name" />
                         </label>
 
-                        <label>
+                        <label className="font-inter text-xl">
                             Nome:
-                            <input type="text" name="name" />
+                            <input className="bg-zinc-300  w-[35rem] ml-[9rem] " type="text" name="name" />
                         </label>
 
-                        <label>
+                        <label className="font-inter text-xl">
                             Evolução:
-                            <input type="text" name="name" />
+                            <input className="bg-zinc-300  w-[35rem]  ml-[7rem]" type="text" name="name" />
                         </label>
-                        <label>
+                        <label className="font-inter text-xl">
                             Numero na Pokedex:
-                            <input type="text" id="algumaCoisa"></input></label>
+                            <input className="bg-zinc-300  w-[35rem]  ml-2 " type="text" id="index-pokedex"></input></label>
 
+                        <Link to={'/'} className="h-1 w-1 mt-10"><button className=" bg-zinc-300 h-5 w-20 flex justify-center items-center font-inter text-lg hover:bg-zinc-400 rounded-sm ">Voltar</button></Link>
                     </label>
-                    <button type="submit" className="flex flex-col-reverse bg-zinc-300 h-5 w-20 mt-[28rem] mr-5" >Enviar</button>
+
+                    <button type="submit" className="flex flex-col-reverse justify-center items-center bg-zinc-300 h-5 w-20 mt-[25.5rem] mr-5 pl-4 hover:bg-zinc-400 font-inter text-lg rounded-sm p-2" >Enviar</button>
+
                 </fieldset>
+
             </form>
 
         </div>
